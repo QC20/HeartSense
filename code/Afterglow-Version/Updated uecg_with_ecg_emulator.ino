3a83120551d8206fb6d8178013ea8655c0cc9772
QC20 <xgv866@alumni.ku.dk>
2024-08-28 12:47:10 +0200

    make new updated 'Afterglow' attempt to show heart rate change

---
+++ code/Afterglow-Version/Updated uecg_with_ecg_emulator.ino
@@ -0,0 +1,53 @@
+#include <uECG.h>
+#include "ECGEmulator.h"
+
+#define PIN 6
+#define NUMPIXELS 12
+
+// This is the correct setup for the following RF-Nano V3.0 clone: 
+int rf_cen = 8; //nRF24 chip enable pin
+int rf_cs = 9; //nRF24 CS pin
+
+ECGEmulator ecg(PIN, NUMPIXELS);
+
+unsigned long lastBeatTime = 0;
+int lastRRid = -1;
+bool playingECG = false;
+unsigned long ecgStartTime = 0;
+
+void setup() {
+  Serial.begin(115200);
+  uECG.begin(rf_cs, rf_cen);
+  ecg.begin();
+}
+
+void loop() {
+  uECG.run();
+  unsigned long currentMillis = millis();
+
+  int currentRRid = uECG.getLastRRid();
+  if (currentRRid != lastRRid) {
+    lastRRid = currentRRid;
+    int bpm = uECG.getBPM();
+    int rr = uECG.getLastRR();
+    
+    Serial.print("BPM: ");
+    Serial.print(bpm);
+    Serial.print(", RR: ");
+    Serial.println(rr);
+    
+    playingECG = true;
+    ecgStartTime = currentMillis;
+  }
+
+  if (playingECG) {
+    int currentBPM = uECG.getBPM();
+    ecg.update(currentMillis - ecgStartTime, currentBPM);
+    
+    if (currentMillis - ecgStartTime >= ECGEmulator::CYCLE_DURATION) {
+      playingECG = false;
+      ecg.setAllLEDs(0);
+      ecg.show();
+    }
+  }
+}
